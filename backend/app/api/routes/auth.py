ccbcb1dcfd98e45a897eb24bd15c111c742a8f9a
ming <1845728120@qq.com>
2026-09-06 21:02:42 +0800

    fix: 限流方法的名称修改

--- backend/app/api/routes/auth.py
+++ backend/app/api/routes/auth.py
@@ -2,7 +2,7 @@
 from fastapi import APIRouter, Depends, HTTPException  
 from app.schemas import auth as auth_schema
 from app.core import captcha
-from app.core.rate_limit import captcha_rate_limit
+from app.core.rate_limit import request_rate_limit
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from app.api.deps import get_db
@@ -20,10 +20,10 @@ router = APIRouter(prefix="/auth", tags=["auth"])
   summary="获取验证码", # 摘要
   response_model=auth_schema.CaptchaSchemaResponse, # 响应模型
   # 依赖可以放在单独的路由，也可以放在APIRouter路由组，还可以直接放在应用级别app = FastAPI(dependencies=[Depends(全站日志)])
-  dependencies=[Depends(captcha_rate_limit)], # 依赖 因为只需要captcha_rate_limit做校验，所以放在这里
+  dependencies=[Depends(request_rate_limit)], # 依赖 因为只需要request_rate_limit做校验，所以放在这里
   responses={429: {"description": "请求过于频繁"}}, # /docs 默认只展示 200（和 422），加这段后 429 也出现在文档里
 )
-# 如果接口函数要使用依赖，就不能放在装饰器的 dependencies 里，而是作为方法形参传入：def get_captcha(limit = Depends(captcha_rate_limit))，然后在函数里用 limit:
+# 如果接口函数要使用依赖，就不能放在装饰器的 dependencies 里，而是作为方法形参传入：def get_captcha(limit = Depends(request_rate_limit))，然后在函数里用 limit:
 def get_captcha():
     """生成验证码信息并返回"""
     return captcha.generate() # 返回id和图片的base64编码
@@ -34,7 +34,7 @@ def get_captcha():
   '/public_key', # 路径  /auth/public_key
   summary="获取公钥", # 摘要
   response_model=auth_schema.PublicKeySchemaResponse, # 响应模型
-  dependencies=[Depends(captcha_rate_limit)], # 依赖 因为只需要captcha_rate_limit做校验，所以放在这里
+  dependencies=[Depends(request_rate_limit)], # 依赖 因为只需要request_rate_limit做校验，所以放在这里
   responses={429: {"description": "请求过于频繁"}}, # /docs 默认只展示 200（和 422），加这段后 429 也出现在文档里
 )
 def get_public_key():
